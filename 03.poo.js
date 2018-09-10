1d3286320ea6329b8b7d4a16ae3de7d87dc20cbd
Flora Calbris <flora.calbris@gmail.com>
2018-09-10 16:33:51 +0200

    03 - poo

--- 03.poo.js
+++ 03.poo.js
@@ -1,13 +1,15 @@
 //PARTIE 3 : la programmation orientée objet
 
-//Fonction constructeur
+var lg = console.log
 
+//Fonction constructeur
 function Personne(
     nom, 
     prenom, 
     pseudo, 
-    getNomComplet(){
-    return nom + prenom + pseudo;})
+    getNomComplet(){return nom + prenom + pseudo;}){}
 
     var Jules = new Personne(Jules, LEMAIRE,jules77);
     var Paul = new Personne(Paul, LEMAIRE, paul44);
+
+    lg(Jules.nom);
